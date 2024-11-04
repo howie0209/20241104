f74078381d4f8c56518e1fad28765fe56e5bd4b4
howie <howie8853x34@gmail.com>
2024-11-04 15:29:01 +0800

    v3_出生體重輸入

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.compose.material3.Button
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.text.input.KeyboardType
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,6 +53,7 @@ class MainActivity : ComponentActivity() {
 
 fun Birth(m: Modifier){
     var userName by remember { mutableStateOf("王浩宇")}
+    var userWeight by remember { mutableStateOf(3800)}
 
     Column {
         TextField(
@@ -64,8 +66,22 @@ fun Birth(m: Modifier){
             placeholder = { Text("請輸入您的姓名") }
 
         )
+        TextField(
+            value = userWeight.toString(),
+            onValueChange = { newText ->
+                if (newText == ""){
+                    userWeight = 0
+                }
+                else{userWeight = newText.toInt()}
+            },
+            label = { Text("出生體重") },
+            keyboardOptions = KeyboardOptions
+                (keyboardType = KeyboardType.Number)
+        )
 
-        Text("您輸入的姓名是：$userName")
+
+        Text("您輸入的姓名是：$userName\n出生體重為：$userWeight 公克")
     }
+
 
 }
